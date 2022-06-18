149ebdf85c25eee1c759e481c2175d1359193d1f
Ianne93 <alessandro.ianniello@hotmail.it>
2022-06-18 08:17:10 +0100

    Ho scritto ciao

--- src/Moto.java
+++ src/Moto.java
@@ -1,5 +1,6 @@
 public class Moto extends VeicoloMotore{
     public Moto(String id, String posizione, String targa, double tariffa, String patenteGuida){
         super(id, posizione, targa, tariffa, patenteGuida);
+        System.out.println("Ciao");
     }
 }
