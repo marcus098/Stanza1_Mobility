c97090a77353e2d818f5733bee03e78416a31aab
marcus098 <43844683+marcus098@users.noreply.github.com>
2022-06-18 09:25:33 +0200

    Prova Push/Pull

--- src/Moto.java
+++ src/Moto.java
@@ -2,5 +2,6 @@ public class Moto extends VeicoloMotore{
 
     public Moto(String id, String posizione, double tariffa, String targa, String patenteGuida) {
         super(id, posizione, tariffa, targa, patenteGuida);
+        System.out.println("Ciao");
     }
 }
